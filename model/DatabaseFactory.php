460cd90c458ed09d69459c3ec99d47f7a23a9bbc
ArtMG <gx.morisson@protonmail.com>
2020-03-06 11:05:54 +0100

    create post class and db factory

---
+++ model/DatabaseFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+require('config.php');
+
+class DatabaseFactory
+{
+    public static function dbConnect()
+    {
+        $db = new PDO('mysql:host='. DB_HOST .';dbname='. DB_NAME, DB_USER, DB_PASS);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+    }
+}
---
+++ model/Post.php
@@ -0,0 +1,131 @@
+<?php
+
+class Post
+{
+    private $id;
+    private $title;
+    private $content;
+    private $status;
+    private $created_at;
+    private $updated_at;
+
+    /**
+     * Post constructor.
+     * @param array $data
+     */
+    public function __construct($data = [])
+    {
+        if (!empty($data)) {
+            $this->hydrate($data);
+        }
+    }
+
+    /**
+     * @param array $data
+     */
+    private function hydrate(array $data)
+    {
+        foreach ($data as $attr => $val) {
+            $setMethod = 'set'.ucfirst($attr);
+            if (is_callable($setMethod)) {
+                $this->$setMethod($val);
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param mixed $updated_at
+     */
+    public function setUpdatedAt($updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
+}

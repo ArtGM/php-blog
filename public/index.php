8235e5b4114d95dd15a0e125e3f3df83f523ec7a
ArtMG <gx.morisson@protonmail.com>
2020-03-13 08:15:00 +0100

    structure, htaccess, post class, database factory and config file

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// use Blog\src\config\Router;
+
+require '../src/config/config.php';
+$loader = require '../vendor/autoload.php';
+$loader->addPsr4('Acme\\Test\\', __DIR__);
+
+
+// $router = new Router();
+
+

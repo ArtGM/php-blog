fee716934e2221d8a82564706c79916e00306f2c
ArtMG <gx.morisson@protonmail.com>
2020-05-10 16:10:27 +0200

    improve slug class

--- src/tools/Slug.php
+++ src/tools/Slug.php
@@ -1,6 +1,5 @@
 <?php
 
-//change namespace
 namespace Blog\src\tools;
 
 class Slug
@@ -8,14 +7,16 @@ class Slug
     public function generate(string $string): string
     {
         $string = preg_replace('/\s+/', '-', mb_strtolower(trim(strip_tags($string)), 'UTF-8'));
-        $string = preg_replace('#à|á|â|ã|ä|å#', 'a', $string);
+        $string = preg_replace('#[àáâãäå]#', 'a', $string);
         $string = preg_replace('#ç#', 'c', $string);
-        $string = preg_replace('#è|é|ê|ë#', 'e', $string);
-        $string = preg_replace('#ì|í|î|ï#', 'i', $string);
-        $string = preg_replace('#ð|ò|ó|ô|õ|ö#', 'o', $string);
-        $string = preg_replace('#ù|ú|û|ü#', 'u', $string);
-        $string = preg_replace('#ý|ÿ#', 'y', $string);
+        $string = preg_replace('#[èéêë]#', 'e', $string);
+        $string = preg_replace('#[ìíîï]#', 'i', $string);
+        $string = preg_replace('#[ðòóôõö]#', 'o', $string);
+        $string = preg_replace('#[ùúûü]#', 'u', $string);
+        $string = preg_replace('#[ýÿ]#', 'y', $string);
         $string = preg_replace('#|_|:|\.|,|\'|"#', '', $string);
+        $string = preg_replace('#\d$#', '', $string);// remove number at the end
+        $string = preg_replace('#\W$#', '', $string);// remove character at the end
         return $string;
     }
 }

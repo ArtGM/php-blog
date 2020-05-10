4bb997c01f689546979746532939787afa1d43bd
ArtMG <gx.morisson@protonmail.com>
2020-05-10 19:44:22 +0200

    rename db to database

--- src/config/DatabaseFactory.php
+++ src/config/DatabaseFactory.php
@@ -6,8 +6,8 @@ class DatabaseFactory
 {
     public static function dbConnect()
     {
-        $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $db;
+        $database = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
+        $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $database;
     }
 }

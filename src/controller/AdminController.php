8c9a8f6445c8f438d304b02af1f7ad384bf7c3ff
ArtMG <gx.morisson@protonmail.com>
2020-07-31 11:22:13 +0200

    fix: access to user profile and hide dashboard.html.twig when not admin fix: css style and layout on form

--- src/controller/AdminController.php
+++ src/controller/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Blog\src\controller;
 
+
 class AdminController extends Controller
 {
     private function checkAdmin()
@@ -122,11 +123,11 @@ class AdminController extends Controller
 
     public function showUserProfile($user_id)
     {
-        if ($this->checkLoggedIn()) {
-            if ($this->checkAdmin()) {
+        if ($this->session->getSession('connected')) {
+            if ($this->session->getSession('role') === '1') {
                 $user_data = $this->user->getUserById($user_id);
             } else {
-                $user_data = $this->session->getSession('id');
+                $user_data = $this->user->getUserById($this->session->getSession('id'));
             }
             $this->render('/admin/profile.html.twig', ['user' => $user_data]);
         }
@@ -134,11 +135,11 @@ class AdminController extends Controller
 
     public function editUserProfile($user_id)
     {
-        if ($this->checkLoggedIn()) {
-            if ($this->checkAdmin()) {
+        if ($this->session->getSession('connected')) {
+            if ($this->session->getSession('role') === '1') {
                 $user_data = $this->user->getUserById($user_id);
             } else {
-                $user_data = $this->session->getSession('id');
+                $user_data = $this->user->getUserById($this->session->getSession('id'));
             }
             $this->render('/admin/edit_profile.html.twig', ['user' => $user_data]);
         }
